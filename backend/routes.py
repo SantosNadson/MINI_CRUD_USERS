0cc0bfc0dc194ff37c98b700075737a729556755
Nadson <nadsonsantossilva17@gmail.com>
2026-03-14 23:15:11 -0300

    Encontrar a pasta frontend a partir do dirtório atual, antes usava-se caminho absoluto, ao executar via terminal gerava erro

--- backend/routes.py
+++ backend/routes.py
@@ -5,16 +5,19 @@ from fastapi import Request
 import json
 import pandas as pd
 import services
+import os
 
-templates = Jinja2Templates(directory='frontend/templates')
-app.mount("/static", StaticFiles(directory='frontend/static'), name="static")
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+templates = Jinja2Templates(directory=os.path.join(ROOT_DIR, 'frontend/templates'))
+app.mount("/static", StaticFiles(directory=os.path.join(ROOT_DIR, 'frontend/static')), name="static")
 
 @app.get("/")
-def home_page(request: Request):
+def homepage(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/home")
-def home_route(request: Request):
+def homepage(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/users")
@@ -27,23 +30,19 @@ def show_data():
     return data
 
 @app.post("/users/insert_dados")
-async def insert_user(request: Request):
-    data = await request.json()
-    services.insert_data(data['nome'], data['idade'])
-    return {'status': 'ok'}
+async def insert_dados(request: Request):
+    dados = await request.json()
+    services.insert_data(dados['nome'],dados['idade'])
+    return {'status':'ok'}
 
 @app.put("/users/update_dados")
-async def update_user(request: Request):
-    data = await request.json()
-    services.update_data(data["user_id"], data["new_name"], data["new_age"])
-    return {"usuário atualizado": "ok"}
+async def update_dados(request: Request):
+    dados = await request.json()
+    services.update_data(dados["user_id"],dados["new_name"],dados["new_age"])
+    return{"usuário atualizado": "ok"}
 
 @app.delete("/users/delete_dados")
-async def delete_user(request: Request):
-    data = await request.json()
-    services.delete_data(data["user_id"])
+async def delete_dados(request: Request):
+    dados = await request.json()
+    services.delete_data(dados["user_id"])
     return {"Usuário removido": "ok"}
-
-
-if __name__ == "__main__":
-    pass
